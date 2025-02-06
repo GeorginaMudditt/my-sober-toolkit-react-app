2102e1a989e32181f3e1a5f95511e698b3d05423
Georgina Mudditt <g.k.mudditt@gmail.com>
2025-02-06 16:23:51 +0100

    add navbar

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,48 @@
 import React from "react";
 import "./Header.css";
 import logo from "./images/sober-toolkit-logo-grey.png";
+import navbar_logo from "./images/toolkit-navbar-icon.png";
 
 function Header() {
   return (
     <div className="Header">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img className="navbar-logo" src={navbar_logo} alt="Navbar logo" />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Resources
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Sober counter
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About this project
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       <img className="main-logo" src={logo} alt="Sober Toolkit logo" />
     </div>
   );
