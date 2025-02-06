342f693edcbe5e3e09b07dcbf4dd13e7fe11d06b
Georgina Mudditt <g.k.mudditt@gmail.com>
2025-02-06 15:55:32 +0100

    change favicon, add logo, import fonts, start styling

--- src/About.js
+++ src/About.js
@@ -4,7 +4,28 @@ import "./About.css";
 function About() {
   return (
     <div className="About">
-      <h1>About</h1>
+      <h1>About this project</h1>
+      <p>
+        If you would like to suggest a resource to add, or give me some
+        feedback, I would love to hear from you!
+      </p>
+      <button>
+        <a href="mailto:g.k.mudditt@gmail.com">Email me</a>
+      </button>
+      <p>
+        If you have found this page useful, please consider making a small
+        donation. I created this project with love, not for money, but being
+        supported would mean the world
+      </p>
+      <button>
+        <a
+          href="https://buymeacoffee.com/sobertoolkit"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Support me
+        </a>
+      </button>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Header.css";
+import logo from "./images/sober-toolkit-logo-grey.png";
 
 function Header() {
   return (
     <div className="Header">
-      <h1>Header</h1>
+      <img className="main-logo" src={logo} alt="Sober Toolkit logo" />
     </div>
   );
 }
