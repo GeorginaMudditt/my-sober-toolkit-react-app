a204d9f640a503db8b0ae1b2a2ab9e49f1fbf697
Georgina Mudditt <g.k.mudditt@gmail.com>
2025-02-18 14:25:28 +0100

    link supabse sober_resources database to project

--- src/Resources.js
+++ src/Resources.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react"; // is this the right place for this?
 import "./Resources.css";
-// import supabase from "./config/supabaseClient";
+import { supabase } from "./config/supabaseClient"; // is this okay here?
 
 function Resources() {
+  const [resources, setResources] = useState([]);
+
+  useEffect(() => {
+    const fetchResources = async () => {
+      const { data, error } = await supabase
+        .from("sober_resources")
+        .select("title, secondary_information");
+      if (error) {
+        console.error("Error fetching resources:", error);
+      } else {
+        setResources(data);
+      }
+    };
+
+    fetchResources();
+  }, []);
+
   return (
     <div id="resources-section" className="Resources">
       <h1>Resources</h1>
@@ -18,6 +35,14 @@ function Resources() {
         <li>Podcasts</li>
         <li>Social media</li>
       </ul>
+      <div className="resource-cards">
+        {resources.map((resource) => (
+          <div key={resource.id} className="resource-card">
+            <h2>{resource.title}</h2>
+            <p>{resource.secondary_information}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,7 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  "https://wwzftqzebnqusslywxid.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3emZ0cXplYm5xdXNzbHl3eGlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0NDQwNjEsImV4cCI6MjA1NTAyMDA2MX0.IYOMvFPexStLcMLeBZARfUeOlFrAIZ_HUnlY-d4-2ks"
+);
 
-export default supabase;
+// REACT_APP_SUPABASE_URL = "https://wwzftqzebnqusslywxid.supabase.co";
+
+// https: REACT_APP_SUPABASE_ANON_KEY =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3emZ0cXplYm5xdXNzbHl3eGlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0NDQwNjEsImV4cCI6MjA1NTAyMDA2MX0.IYOMvFPexStLcMLeBZARfUeOlFrAIZ_HUnlY-d4-2ks";
+
+// const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+// const supabaseKey = process.env.REACT_APP_ANON_KEY;
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export default supabase;
